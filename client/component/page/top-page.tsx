ef1c1aedd0203b25819f72ac25268fe2799f13ba
Ziphil <yfe23457@nifty.com>
2021-08-29 22:41:04 +0900

    トップページにユーザーページなどへのリンクを追加

--- client/component/page/top-page.tsx
+++ client/component/page/top-page.tsx
@@ -4,6 +4,9 @@ import * as react from "react";
 import {
   ReactNode
 } from "react";
+import {
+  Link
+} from "react-router-dom";
 import Component from "/client/component/component";
 import DialectList from "/client/component/compound/dialect-list";
 import {
@@ -16,8 +19,33 @@ import Page from "/client/component/page/page";
 export default class TopPage extends Component<Props, State> {
 
   public render(): ReactNode {
+    let userPageNode = (
+      <Link to={`/cla/${this.props.store!.user?.code}`}>
+        <div styleName="circle">
+          <div styleName="icon">&#xF007;</div>
+          <div styleName="label">{this.trans("topPage.userPage")}</div>
+        </div>
+      </Link>
+    );
+    let loginNode = (
+      <Link to="/login">
+        <div styleName="circle">
+          <div styleName="icon">&#xF2F6;</div>
+          <div styleName="label">{this.trans("topPage.login")}</div>
+        </div>
+      </Link>
+    );
     let node = (
       <Page>
+        <div styleName="circle-list">
+          <Link to="/about">
+            <div styleName="circle">
+              <div styleName="icon">&#xF128;</div>
+              <div styleName="label">{this.trans("topPage.about")}</div>
+            </div>
+          </Link>
+          {(this.props.store!.user !== null) ? userPageNode : loginNode}
+        </div>
         <div styleName="list">
           <DialectList approved={true}/>
         </div>
