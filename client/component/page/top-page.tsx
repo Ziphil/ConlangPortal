03a76275ed84fbc0ffc9db30b70baea87976285e
Ziphil <yfe23457@nifty.com>
2021-09-04 18:38:30 +0900

    リファクタリング

--- client/component/page/top-page.tsx
+++ client/component/page/top-page.tsx
@@ -20,29 +20,23 @@ export default class TopPage extends Component<Props, State> {
 
   public render(): ReactNode {
     let userPageNode = (
-      <Link to={`/cla/${this.props.store!.user?.code}`}>
-        <div styleName="circle">
-          <div styleName="icon">&#xF007;</div>
-          <div styleName="label">{this.trans("topPage.userPage")}</div>
-        </div>
+      <Link styleName="circle" to={`/cla/${this.props.store!.user?.code}`}>
+        <div styleName="icon">&#xF007;</div>
+        <div styleName="label">{this.trans("topPage.userPage")}</div>
       </Link>
     );
     let loginNode = (
-      <Link to="/login">
-        <div styleName="circle">
-          <div styleName="icon">&#xF2F6;</div>
-          <div styleName="label">{this.trans("topPage.login")}</div>
-        </div>
+      <Link styleName="circle" to="/login">
+        <div styleName="icon">&#xF2F6;</div>
+        <div styleName="label">{this.trans("topPage.login")}</div>
       </Link>
     );
     let node = (
       <Page>
         <div styleName="circle-list">
-          <Link to="/about">
-            <div styleName="circle">
-              <div styleName="icon">&#xF128;</div>
-              <div styleName="label">{this.trans("topPage.about")}</div>
-            </div>
+          <Link styleName="circle" to="/about">
+            <div styleName="icon">&#xF128;</div>
+            <div styleName="label">{this.trans("topPage.about")}</div>
           </Link>
           {(this.props.store!.user !== null) ? userPageNode : loginNode}
         </div>
